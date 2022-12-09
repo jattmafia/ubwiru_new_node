9f9332808d21f7de5eb60609ff5da56e77068ca3
Jassi <95351771+jattmafia@users.noreply.github.com>
2022-12-09 11:35:53 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3030;
 
 
 app.use(express.static('uploads'));
@@ -22,7 +23,7 @@ mongoose.connect("mongodb+srv://ubwiru:gUjhe1uC7ZnaKuIB@cluster0.c1roijk.mongodb
   });
 
 
- let PORT;
+
  app.listen(PORT, function() {
     {}
     console.log(`App started at : ${PORT}`);
