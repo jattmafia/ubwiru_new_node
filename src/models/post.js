01ab9012589ddd047e708ade49f5fcd4f559caec
Jassi <95351771+jattmafia@users.noreply.github.com>
2022-12-13 12:46:26 +0530

    Update post.js

--- src/models/post.js
+++ src/models/post.js
@@ -2,8 +2,8 @@ const {Schema,model,mongoose} = require('mongoose');
 const {ObjectId} = mongoose.Schema.Types;
 
 const postSchema = new Schema({
-    title:{type:String,required:true},
-    body:{type: String,required : true},
+    title:{type:String},
+    body:{type: String},
     image:{type: String,},
     postedBy:{type:ObjectId,ref:"user"}
 })
